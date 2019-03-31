8962759fa8fc6701502236db06d7e7e32182aff6
Jeff Cameron <jecameron@expedia.com>
2019-03-31 11:24:52 -0700

    Implemented a sampler for the Bradley Terry model.

--- bradley_terry.py
+++ bradley_terry.py
@@ -1,3 +1,21 @@
+import random
 
-def f(x):
-    return 3 * x + 1;
+# The classic Bradley-Terry model.
+#   a - the skill level of player A.
+#   b - the skill level of player B.
+# Returns True if A wins, False if B wins. The random probability is based on
+# a coin flip with probability p = a / (a + b). There are no draws allowed; one
+# player or the other must win.
+def GenerateOneRandomGameOutcome(a, b):
+    return random.random() < float(a) / float(a + b)
+
+# Generate a list of game outcomes, in the form [(winner_id, loser_id), ...]
+def GenerateRandomGameOutcomes(skill_levels, n):
+    outcomes = []
+    for i in range(n):
+        j, k = random.sample(range(len(skill_levels)), 2)
+        if GenerateOneRandomGameOutcome(skill_levels[j], skill_levels[k]):
+            outcomes.append((j, k))
+        else:
+            outcomes.append((k, j))
+    return outcomes
--- bradley_terry_test.py
+++ bradley_terry_test.py
@@ -1,25 +1,40 @@
+# Some of these tests use random numbers, so technically could be flaky.
+# Flakiness has been avoided by selecting extremely wide bounds for the
+# assertions that are beyond the reach of ordinary probability.
 import unittest
 
 import bradley_terry
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_random_game_outcome(self):
+        games = 1000
+        wins = 0
+        for i in range(games):
+            if bradley_terry.GenerateOneRandomGameOutcome(3, 1):
+                wins += 1
+        # The win rate should be between 50% and 100%.
+        self.assertTrue(wins > games / 2)
+        self.assertTrue(wins < games)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
-    def test_imported_function(self):
-        self.assertEqual(bradley_terry.f(2), 7)
+    def test_generate_random_game_outcomes(self):
+        trials = 1000
+        skills = [1, 1, 2]
+        outcomes = bradley_terry.GenerateRandomGameOutcomes(skills, trials)
+        # Sanity check that winner != loser.
+        self.assertNotEqual(outcomes[0][0], outcomes[0][1])
+        # Count the number of wins by player.
+        wins = [0, 0, 0]
+        for winner, loser in outcomes:
+            wins[winner] += 1
+        # Each player should win some games.
+        self.assertTrue(wins[0] > 0)
+        self.assertTrue(wins[1] > 0)
+        self.assertTrue(wins[2] > 0)
+        # Player 2 should win more games due to higher skill.
+        self.assertTrue(wins[2] > trials / 3)
+        # The wins should add up to the total number of games played.
+        self.assertEqual(sum(wins), trials)
 
 if __name__ == '__main__':
     unittest.main()
